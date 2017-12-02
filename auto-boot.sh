fc0d51601901f1cdc1549612fdaac3a36d2c52b2
ShenShan123 <yolkinnng@gmail.com>
2017-12-02 13:40:43 +0800

    modified:   auto-boot.sh 	modified:   parsetab.py 	modified:   parsetab.pyc 	new file:   tonto/m5out/config.ini 	new file:   tonto/m5out/config.json 	new file:   tonto/m5out/cpt.None.2099998889/m5.cpt 	new file:   tonto/m5out/cpt.None.2099998889/system.physmem.store0.pmem 	new file:   tonto/m5out/cpt.simpoint_00_inst_1298899998_weight_0.480122_interval_1_warmup_0/m5.cpt 	new file:   tonto/m5out/cpt.simpoint_00_inst_1298899998_weight_0.480122_interval_1_warmup_0/system.physmem.store0.pmem 	new file:   tonto/m5out/cpt.simpoint_01_inst_2099998889_weight_0.275229_interval_1_warmup_0/m5.cpt 	new file:   tonto/m5out/cpt.simpoint_01_inst_2099998889_weight_0.275229_interval_1_warmup_0/system.physmem.store0.pmem 	new file:   tonto/m5out/stats.txt 	new file:   tonto/m5out/tonto-simpoint.bb 	new file:   tonto/metrics-dump.txt 	new file:   tonto/pin.log 	new file:   tonto/rdv-16384.txt 	new file:   tonto/stdin 	new file:   tonto/stdout 	new file:   tonto/tonto_base.gcc41-amd64bit

--- auto-boot.sh
+++ auto-boot.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
 ../build/x86/gem5.fast ../configs/example/se.py \
---caches --l1i_size=32kB --l1i_assoc=8 --l1d_size=32kB --l1d_assoc=8 \
---l2cache --l2_assoc=8 --l2_size=1MB --num-l2caches=1 \
---mem-size=1024MB \
---cpu-type=arm_detailed \
+--fastmem \
+--cpu-type=AtomicSimpleCPU \
+--simpoint-profile --simpoint-interval=10000000 \
 -c ./tonto_base.gcc41-amd64bit
 #-c ./perlbench_base.gcc41-amd64bit -o "-I./lib checkspam.pl 2500 5 25 11 150 1 1 1 1"
 #-c ./bwaves_base.gcc41-amd64bit
 #-c ./bzip2_base.gcc41-amd64bit -o "chicken.jpg 30"
 #
+# take checkpoints from Simpoint files
+../build/x86/gem5.fast ../configs/example/se.py \
+--mem-size=1024MB --fastmem \
+--cpu-type=AtomicSimpleCPU \
+--take-simpoint-checkpoint=../SimPoint.3.2/output/tonto.simpoints,../SimPoint.3.2/output/tonto.weights,1,0 \
+-c ./tonto_base.gcc41-amd64bit
+# restore from the simpoint checkpoints
+../build/x86/gem5.fast ../configs/example/se.py \
+--caches --l1i_size=32kB --l1i_assoc=8 --l1d_size=32kB --l1d_assoc=8 \
+--l2cache --l2_assoc=8 --l2_size=4MB --num-l2caches=1 \
+--mem-size=1024MB \
+--cpu-type=DerivO3CPU \
+-r 2099998889 --at-instruction \
+-c ./tonto_base.gcc41-amd64bit
+
+../build/x86/gem5.fast ../configs/example/se.py \
+--caches --l1i_size=32kB --l1i_assoc=4 --l1d_size=32kB --l1d_assoc=4 \
+--l2cache --l2_assoc=16 --l2_size=4MB --num-l2caches=1 \
+--mem-size=1024MB \
+--cpu-type=DerivO3CPU \
+-r 2099998889 --at-instruction \
+-c ./tonto_base.gcc41-amd64bit
